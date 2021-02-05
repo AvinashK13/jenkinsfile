f5dc448fe611b6f5b3b21a61aebf61e3718d8f9b
avinash <avinash.kademani13@gmail.com>
2021-02-05 14:13:31 +0000

    first commit

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,34 @@
+pipeline {
+    agent any
+    stages {
+        stage('Checkout') {
+            steps {
+                echo '##Getting the code from the Github##'
+                checkout([$class: 'GitSCM', branches: [[name: '*/main']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/AvinashK13/cdac-demo']]])
+            }
+        }
+        stage('Build Demo Application') {
+            steps {
+                echo '##Building the Demo application##'
+                sh '''
+                    rm -rf /home/ubuntu/bkp/*
+                    cp $WORKSPACE/demo.js /home/ubuntu/bkp/
+                '''
+            }
+        }
+                stage('Build Approval') {
+            steps {
+              input "Do you want to proceed for build ?"
+            }
+        }
+        stage('Deploy Demo Application on Web node') {
+            steps {
+                echo '##Deploying Demo Application on Web node##'
+                sh '''
+                    cd /opt/ansible_script/plays/
+                    ansible-playbook cdacdemo.yml
+                '''
+            }
+        }
+    }
+ }
